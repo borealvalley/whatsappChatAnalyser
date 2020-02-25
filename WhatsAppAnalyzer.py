bc8dc3ca4bda95c63d3b8607c8a288f95746e5e1
Hoang An Nguyen <an_nguyen@hotmail.de>
2020-02-25 16:14:55 +0100

    add functions of visualisation

--- WhatsAppAnalyzer.py
+++ WhatsAppAnalyzer.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 import re 
@@ -8,7 +9,7 @@ def getFilePath() :
     filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
     return filename
 
-#open File and get its contents split in lists
+#open File and get its contents split in lists with names and the number of
 def getNames(path) :
     file = open(path, "r", encoding="utf8")
     dictnames = {} 
@@ -21,8 +22,24 @@ def getNames(path) :
                 dictnames[name] = dictnames[name] + 1
             else:
                 dictnames[name] = 1
-            
     print(dictnames)
+    return dictnames
 
+def makePieChart(dictionary) :
+    listnames = []
+    values = []
+    for key in dictionary:
+        listnames.append(key)
+    for nummsg in dictionary.values():
+        values.append(nummsg)
+    
+    plt.pie(values, labels=listnames, autopct='%1.1f%%', shadow=True, startangle=140)
+    plt.axis('equal')
+    plt.legend(listnames, loc="best")
+    plt.show()
+    print(listnames)
+    print(values)
+
+# heres the main
 pathToFile = getFilePath()
-getNames(pathToFile)
+makePieChart(getNames(pathToFile))
